c76e6a661350692bb503c98c484dda5ade694584
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-24 21:27:48 -0500

    Completed python algo

--- python/linear_search.py
+++ python/linear_search.py
@@ -1,6 +1,19 @@
-array_to_search_through = []
-for number in range(1, 1001):
-    array_to_search_through.append(number)
+# array_to_search_through = []
+# for number in range(1, 1001):
+#     array_to_search_through.append(number)
 
 def linear_search(value_to_find, array_to_search_through):
-    # your code here
+    x = value_to_find
+    y = array_to_search_through
+    z = []
+
+    for i in range(0, len(y)):
+        if(x == y[i]):
+            z.append(i)
+
+    if(len(z) == 0):
+        return None
+    elif(len(z) == 1):
+        return z[0]
+    else:
+        return z
